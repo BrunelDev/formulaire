1fc75e56caf50b3251eda1518a3040554266fa3e
BrunelDev <152787382+BrunelDev@users.noreply.github.com>
2025-10-17 21:12:35 +0100

    fix d'erreurs de builds

--- app/api/generate-pdf/route.ts
+++ app/api/generate-pdf/route.ts
@@ -45,12 +45,6 @@ export async function POST(request: NextRequest) {
     const pdf = await page.pdf({
       format: "A4",
       printBackground: true,
-      margin: {
-        top: "20px",
-        bottom: "20px",
-        left: "20px",
-        right: "20px",
-      },
     });
 
     await browser.close();
