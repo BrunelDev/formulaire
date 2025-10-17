a9819bef3d6f9cf38c06df2bdec0f38c326f147a
BrunelDev <152787382+BrunelDev@users.noreply.github.com>
2025-10-17 20:14:51 +0100

    fix d'erreurs de builds

--- app/api/generate-pdf/route.ts
+++ app/api/generate-pdf/route.ts
@@ -1,4 +1,4 @@
-import chromium from "@sparticuz/chromium-min"
+import chromium from "@sparticuz/chromium"
 import { NextRequest, NextResponse } from "next/server";
 import puppeteer from "puppeteer-core";
 
