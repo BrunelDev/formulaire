0029f71c60895b22716ee6c5ca743a2e1a87bf84
BrunelDev <152787382+BrunelDev@users.noreply.github.com>
2025-11-04 15:35:17 +0100

    ajout du lien vers nos offres

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -98,7 +98,7 @@ export default function NavBar() {
       >
         <div className="flex flex-col w-full lg:w-[77%] items-center justify-center absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2">
           <div className="flex items-center justify-between relative w-full">
-            <Link href={" https://mesplansdepermis.fr/nos-offres/ "}>
+            <Link href={" https://mesplansdepermis.fr"}>
               <Image
                 className="relative hidden lg:block w-[100px] sm:w-[120px] lg:w-[127px] h-7 sm:h-8 lg:h-[35px] object-cover"
                 width={129.73}
@@ -114,7 +114,7 @@ export default function NavBar() {
               alt="Logo image"
               onClick={() => {
                 window.location.href =
-                  "https://mesplansdepermis.fr/nos-offres/";
+                  "https://mesplansdepermis.fr";
               }}
               src={"/images/logoNoText.jpg"}
             />
